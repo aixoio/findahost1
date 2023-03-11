6b9e1d6e208bf2c17bb0216c10491b5f12405020
aixoio <79969530+aixoio@users.noreply.github.com>
2023-03-11 11:04:35 +1100

    Add the add your location button

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,9 @@
         <input type="number" placeholder="Lat" class="bg-transparent border-b border-b-black placeholder:text-gray-300 m-2 p-2 rounded focus:outline-none" v-model="serverLocat.lat">
         <input type="number" placeholder="Lon" class="bg-transparent border-b border-b-black placeholder:text-gray-300 m-2 p-2 rounded focus:outline-none" v-model="serverLocat.lon">
 
-        <button class="block m-auto mt-4 bg-gradient-to-br from-pink-400 via-purple-400 to-indigo-500 p-2 rounded transition-all ease-linear hover:scale-110 duration-150" @click="continuebtn">Continue</button>
+        <button class="block m-auto mt-4 bg-gradient-to-br from-pink-400 via-purple-400 to-indigo-500 p-2 rounded transition-all ease-linear hover:scale-110 duration-150 shadow" @click="continuebtn">Continue</button>
 
-        <button class="block m-auto mt-8 bg-slate-900 p-2 rounded cursor-pointer transition-all ease-in hover:scale-110 duration-150" @click="skipbtn">Skip</button>
+        <button class="block m-auto mt-8 bg-slate-900 p-2 rounded cursor-pointer transition-all ease-in hover:scale-110 duration-150 shadow" @click="skipbtn">Skip</button>
 
       </div>
 
@@ -30,6 +30,16 @@
           <option value="none">None</option>
           <option v-for="cdn in allCdnNames" :key="cdn" :value="cdn">{{ cdn }}</option>
         </select>
+        <div v-if="!addedyourlocaiton && !erroraddinglocaiton">
+          <button class="block bg-gradient-to-r from-cyan-300 via-sky-400 to-blue-500 p-2 m-auto rounded mt-2 mb-2 shadow transition-all ease-in hover:scale-110 duration-150" @click="addyourlocationbtn">Add your location</button>
+        </div>
+        <div v-else-if="erroraddinglocaiton">
+          <div class="m-auto bg-red-500 text-white font-bold w-fit p-2 rounded shadow">
+
+            <span>Error adding your location!</span>
+
+          </div>
+        </div>
 
         
       </div>
@@ -72,7 +82,9 @@ export default {
       cuurentCdn: "none",
       cdnMarkers: [],
       map: null,
-      cdnInfo: cdnData
+      cdnInfo: cdnData,
+      addedyourlocaiton: false,
+      erroraddinglocaiton: false
 
     }
 
@@ -153,6 +165,43 @@ export default {
           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       }).addTo(map);
 
+
+    },
+
+    addyourlocationbtn() {
+
+      
+      if ("geolocation" in navigator) {
+        
+        navigator.geolocation.getCurrentPosition((pos) => {
+          
+          const lat = pos.coords.latitude;
+          const lon = pos.coords.longitude;
+          
+          let marker = L.marker([lat, lon]).addTo(this.map)
+          
+          marker.bindPopup("Your location")
+
+          let circle = L.circle([lat, lon], {
+
+            color: "#312e81",
+            fillColor: "#4338ca",
+            fillOpacity: 0.5,
+            radius: pos.coords.accuracy
+
+          }).addTo(this.map)
+
+          circle.bindPopup("Your locaiton")
+
+          this.addedyourlocaiton = true;
+          
+        }, () => this.erroraddinglocaiton = true)
+
+      } else {
+
+        this.erroraddinglocaiton = true;
+
+      }
 
     }
     
